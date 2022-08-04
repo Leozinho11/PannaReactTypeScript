27c4cf51cc247d643d0fb003ce04a1b6f1acf3f7
Samuka <samukasamuel2005@outlook.com>
2022-08-04 08:39:06 -0300

    feat: iniciando criação da sidebar

---
+++ panna/src/components/topbar/contexts/sidebar.types.ts
@@ -0,0 +1,4 @@
+export interface IMenus {
+  title: string
+  routeName: string
+}
---
+++ panna/src/components/topbar/mocks/menus.tsx
@@ -0,0 +1,19 @@
+interface Menus {
+  title: string
+  path: string
+}
+
+export const mockMenus: Menus[] = [
+  {
+    title: 'Home',
+    path: '/home'
+  },
+  {
+    title: 'Jogos',
+    path: '/jogos'
+  },
+  {
+    title: 'Tabelas',
+    path: '/tabelas'
+  }
+]
